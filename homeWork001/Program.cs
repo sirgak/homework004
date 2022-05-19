380eeb35e72414e139a5e4a2c9051b4a04484c92
sirgak <life_4_ever@mail.ru>
2022-05-19 15:59:59 +0300

    test.
    test.

---
+++ homeWork001/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace homeWork001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string name = "Yan";
+            uint age = 27;
+            byte weight = 60;
+            short health = 100;
+            float height = 1.75f;
+            double reach = 1.72;
+            long skill = 0;
+            int balance = -10;
+            char sex = 'M';
+            bool haveArmour? = false;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(weight);
+            Console.WriteLine(height);
+            Console.WriteLine(health);
+            Console.WriteLine(reach);
+            Console.WriteLine(skill);
+            Console.WriteLine(balance);
+            Console.WriteLine(sex);
+            Console.WriteLine(haveArmour);
+        }
+    }
+}
